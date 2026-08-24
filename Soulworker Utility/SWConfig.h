1afb03c980b91ada0a11b5236c0976e1fe8d0bc8
0xarray <41271741+0xarray@users.noreply.github.com>
2026-08-24 16:39:18 +0200

    Bump version to 1.7.1.12

--- Soulworker Utility/SWConfig.h
+++ Soulworker Utility/SWConfig.h
@@ -2,7 +2,7 @@
 #define MAJORNUMBER 1
 #define MINORNUMBER 7
 #define BUILDNUMBER 1
-#define MODIFICATIONNUMBER 11
+#define MODIFICATIONNUMBER 12
 #define STR(value) #value
 #define STRINGIZE(value) STR(value)
 #define APP_VERSION \
